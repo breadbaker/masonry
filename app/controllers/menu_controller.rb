8dcc7e0f1671bf76687733be0bfdf61efdca1f49
App Academy Student <student@appacademy.io>
2013-12-31 16:22:05 -0500

    a2

--- app/controllers/menu_controller.rb
+++ app/controllers/menu_controller.rb
@@ -4,4 +4,8 @@ class MenuController < ApplicationController
     @menu = File.read(Rails.public_path+'/menu.json')
     @special = Special.last.name if Special.last
   end
+
+  def menu
+    @menu = File.read(Rails.public_path+'/menufull.json')
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Masonry::Application.routes.draw do
 
   root to: 'menu#index'
 
+  get '/menu.html', to: 'menu#menu'
+
+  get '/index.html', to: 'menu#index'
+
   get 'twilio', to: 'inbound#twilio'
 end
