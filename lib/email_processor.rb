c5f953d1a6b46ac8bcb33ed9b3cfb1c42a17bf1e
App Academy Student <student@appacademy.io>
2014-01-01 14:15:27 -0500

    process

--- lib/email_processor.rb
+++ lib/email_processor.rb
@@ -1,5 +1,6 @@
 class EmailProcessor
   def self.process(email)
+    Post.create!({ body: email.body, email: email.from })
     # all of your application-specific code here - creating models,
     # processing reports, etc
   end
